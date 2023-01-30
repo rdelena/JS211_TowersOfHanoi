0266f4b4ec75f459d7502c069e7ae5e389f0824f
rdelena <rodnydelena@gmail.com>
2023-01-29 23:30:18 -0600

    added logic to index.js

--- index.js
+++ index.js
@@ -1,40 +1,63 @@
-// Changed from const to let. Due to the const error.
+// declaring stone to null means we do not have a stone in hand
 let stone = null;
 
-// this function is called when a row is clicked.
-// Open your inspector tool to see what is being captured and can be used.
-
 const selectRow = (row) => {
-  console.log("pre stone", stone);
+  // Condition if the stone is null, pick up a stone
   if (stone === null) {
-    pickUpStone(row.id);
+    pickUpStone(row);
   } else {
-    dropStone(row.id, stone);
-    stone = null;
+    // This calls to check if dropping the stone is legal, if so drop the stone.
+    if (isLegal(row, stone)) {
+      dropStone(row, stone);
+      // Setting the stone back to null will allow another stone to be picked up
+      stone = null;
+      // Check for win when a stone is dropped.
+      checkForWin();
+      // If an illegal move is made, logs it on the console, does not let the stone drop to that row.
+    } else {
+      console.log(
+        "Illegal Move! You cannot place a larger stone on top of a smaller stone."
+      );
+    }
   }
-  console.log("post stone", stone);
-
-  const currentRow = row.getAttribute("data-row");
-
-  console.log("Yay, we clicked an item", row);
-  console.log("Here is the stone's id: ", row.id);
-  console.log("Here is the stone's data-size: ", currentRow);
 };
-
-// this function can be called to get the last stone in the stack
-// but there might be something wrong with it...
-const pickUpStone = (rowID) => {
-  const selectedRow = document.getElementById(rowID);
-  stone = selectedRow.removeChild(selectedRow.lastElementChild);
-  console.log(stone);
+// This picks up the last elementChild from the row selected. Only one stone is picked up.
+const pickUpStone = (row) => {
+  if (row.lastElementChild) {
+    stone = row.removeChild(row.lastElementChild);
+    console.log(stone);
+    console.log(row);
+  }
 };
-
-// You could use this function to drop the stone but you'll need to toggle between pickUpStone & dropStone
-// Once you figure that out you'll need to figure out if its a legal move...
-// Something like: if(!stone){pickupStone} else{dropStone}
-
-const dropStone = (rowID, stone) => {
-  document.getElementById(rowID).appendChild(stone);
+// drop Stone takes in two arguments of row and stone to drop the stone picked up onto a row.
+const dropStone = (row, stone) => {
+  row.appendChild(stone);
 };
-
-// * Remember you can use your logic from 'main.js' to maintain the rules of the game. But how? Follow the flow of data just like falling dominoes.
+//
+const isLegal = (row, stone) => {
+  // current size of the stone is stored in the variable
+  const currentSize = parseInt(stone.getAttribute("data-size"));
+  // Condition checks the last element child of the row.
+  if (row.lastElementChild) {
+    const lastStoneSize = parseInt(
+      row.lastElementChild.getAttribute("data-size")
+    );
+    // If the last stone size is bigger than the current held stone, return true so the player may drop the held stone.
+    return lastStoneSize > currentSize;
+  }
+  return true;
+};
+// This checks if all stones are placed on the top row
+const checkForWin = () => {
+  const topRow = document.getElementById("top-row");
+  if (
+    topRow.childElementCount === 4 &&
+    // Check if the last element child is 1 making sure no stone is misplaced
+    topRow.lastElementChild.getAttribute("data-size") === "1"
+  ) {
+    // timeout delay for better alert
+    setTimeout(() => {
+      alert("Congratulations! You won the game!");
+    }, 500);
+  }
+};
